173a51f453ef286bbdbc6d52d08ebb69c05dd545
wcyat <9657150-wcyat@users.noreply.gitlab.com>
2021-10-08 13:27:41 +0000

    update

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def create(dir, link, update, context):
   os.system("mkdir " + dir + " && cd " + dir + " && touch index.html")
   if files.checkexist(dir + "/index.html"):
     os.remove(dir + "/index.html")
-  files.appendfile(dir + "/index.html", "<html><body onload=\"window.location.href=\'" + link + "\'\"></body></html>")
+  files.appendfile(dir + "/index.html", "<!DOCTYPE html><html lang=\"en-US\"><body onload=\"window.location.href=\'" + link + "\'\"></body></html>")
   push()
   context.bot.send_message(chat_id=owner, text="Done.")
 def remove(dir, update, context):
