19e8abd73fb42c917fff023cbf07b50b992c5273
wcyat <9657150-wcyat@users.noreply.gitlab.com>
2021-10-25 13:12:32 +0000

    Format code with yapf, prettier, black, autopep8 and isort
    This commit fixes the style issues introduced in 79e70c0 according to the output
    from yapf, prettier, black, autopep8 and isort.
    
    Details: https://deepsource.io/gl/wcyat/r.wcyat.me/transform/1e5f647a-8d1b-46d4-a7a6-8dfe09f924b6/

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # r.wcyat.me
+
 Code for [r.wcyat.me](https://r.wcyat.me), redirection site. Automated with telegram bot.
 
 You should need to edit some code, at least push.sh and add your token (and user id) in main.py.
 
 IT MUST BE RUN ON LINUX / UNIX
+
 # Usage
+
 First, execute the code:
+
 ```
 python3 main.py
 ```
+
 You need to have pip and python3 installed.
 
 ## Telegram bot
+
 <pre>
 /create path link: create a directory containing an index.html which redirects to the link.
 For root directory, use:  /create . link
--- main.py
+++ main.py
@@ -46,7 +46,9 @@ def create(dir, link, update, context):
         os.remove(dir + "/index.html")
     files.appendfile(
         dir + "/index.html",
-        '<!DOCTYPE html><head><meta charset="UTF-8"><meta http-equiv="refresh" content="0; url=' + link + '" /></head><html lang="en-US"><body></body></html>',
+        '<!DOCTYPE html><head><meta charset="UTF-8"><meta http-equiv="refresh" content="0; url='
+        + link
+        + '" /></head><html lang="en-US"><body></body></html>',
     )
     push()
     context.bot.send_message(chat_id=owner, text="Done.")
