798929cd5c8e1b5accc987a660cf62b66edc5723
wcyat <9657150-wcyat@users.noreply.gitlab.com>
2021-11-08 20:33:24 +0800

    check if url is valid

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -3,8 +3,20 @@ function getvar(variable) {
     let vars = query.split("&");
     for (let i = 0; i < vars.length; i++) {
         let pair = vars[i].split("=");
-        if (pair[0] === variable) {
-            return pair[1];
+        if (pair[0] === variable) {    
+            const regexp = new RegExp('^(https?:\\/\\/)?'+ // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+            '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+            if (regexp.test(pair[1])) {
+                return pair[1];
+            }
+            let invalid = document.createElement("p");
+            invalid.innerHTML = "Invalid URL.";
+            document.body.insertBefore(invalid, document.body.firstChild);
+            return false;
         }
     }
     let notfound = document.createElement("p");
